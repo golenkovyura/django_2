da06ca07f32346a118ae9eab35ea6b0613d7312c
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-09-22 11:51:52 +0300

    Код после урока

--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата и время обновления', auto_now=True)
