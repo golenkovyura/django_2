616adea2cfce3069d99421aec38c8c7f717a415c
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-09-23 21:14:22 +0300

    код после

--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.html import format_html
+from django.contrib import admin
 
 
 class Advertisement(models.Model):
@@ -8,6 +11,15 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время обновления', auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
