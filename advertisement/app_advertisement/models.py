e85ef73345564e4c756fe4143c98d5becf856ead
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-09-24 12:43:26 +0300

    Код после 4 урока с ДЗ

--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.utils.html import format_html
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
@@ -11,6 +14,7 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время обновления', auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -20,6 +24,15 @@ class Advertisement(models.Model):
                 '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата последнего обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            update_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', update_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
